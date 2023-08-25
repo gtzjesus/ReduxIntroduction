26c3c9c8d5be97dbcc92b5f6cd502d512d22b5bd
gtz.jesus <gtz.jesus@outlook.com>
2023-08-25 15:18:07 -0600

    Adding more State, rootReducer, Customer

--- src/store.js
+++ src/store.js
@@ -1,11 +1,17 @@
-import { createStore } from 'redux';
-const initialState = {
+import { combineReducers, createStore } from 'redux';
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: '',
 };
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+  fullName: '',
+  nationalID: '',
+  createdAt: '',
+};
+
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case 'account/deposit':
       return { ...state, balance: state.balance + action.payload };
@@ -31,16 +37,62 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer);
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case 'customer/createCustomer':
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case 'customer/updateName':
+      return { ...state, fullName: action.payload };
+    default:
+      return state;
+  }
+}
 
-store.dispatch({ type: 'account/deposit', payload: 500 });
-store.dispatch({ type: 'account/withdraw', payload: 200 });
-
-store.dispatch({
-  type: 'account/requestLoan',
-  payload: { amount: 1000, purpose: 'Buy a car' },
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
 });
-console.log(store.getState());
 
-store.dispatch({ type: 'account/payLoan' });
+const store = createStore(rootReducer);
+
+function deposit(amount) {
+  return { type: 'account/deposit', payload: amount };
+}
+function withdraw(amount) {
+  return { type: 'account/withdraw', payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return {
+    type: 'account/requestLoan',
+    payload: { amount: amount, purpose: purpose },
+  };
+}
+function payLoan() {
+  return { type: 'account/payLoan' };
+}
+
+store.dispatch(deposit(20000));
+store.dispatch(withdraw(5000));
+store.dispatch(requestLoan(2500, 'Down Payment'));
+store.dispatch(payLoan());
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: 'customer/createCustomer',
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateName(fullName) {
+  return { type: 'customer/updateName', payload: fullName };
+}
+
+store.dispatch(createCustomer('Jesus Gutierrez', '5869302943'));
+console.log(store.getState());
+store.dispatch(updateName('Jonas'));
 console.log(store.getState());
